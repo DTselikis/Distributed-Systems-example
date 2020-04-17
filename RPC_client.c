b26d2418e817f11a136a696718d9c1280bc213b2
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2020-04-17 18:42:01 +0300

    Combine socket and RPC server

--- RPC_client.c
+++ RPC_client.c
@@ -8,6 +8,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "RPC.h"
 
 #define MAX_CONN 128
 // According to manual of listen(2), the default number of maximum
@@ -25,12 +26,9 @@
 void *clientThread(void *arg);
 int serverInitialize(unsigned int port);
 short int checkAvailability(unsigned short int *threadsState, unsigned int size);
-void server(unsigned int port);
+void server(int argc, char *argv[]);
 
 int *strToIntArray(char *floatBuffer, unsigned int numOfElements);
-float findAverage(int *numArray, int numOfElements);
-int *findMinMax(int *numArray, unsigned int numOfElements);
-float *mulMatrixWithFloat(int *numArray, unsigned int numOfElements, float multiplier);
 char *numArrayToCharArray(void *voidArray, unsigned int numOfElements, char delimeter, unsigned int *length, unsigned short int mode);
 char *compileStrArray(char **strArray, unsigned int length, unsigned int numOfElements, char delimeter);
 
@@ -38,12 +36,13 @@ char *compileStrArray(char **strArray, unsigned int length, unsigned int numOfEl
 struct threadArgs {
   int client_discr;
   unsigned short int *finished;
+	char *RPC_Host;
 };
 
 
 int main (int argc, char *argv[]) {
 
-  server(atoi(argv[1]));
+  server(argc, argv);
 
   return 0;
 }
@@ -59,7 +58,7 @@ int main (int argc, char *argv[]) {
  * All threads are set as detachables.
  * Server check for new connections every 2 seconds
 */
-void server(unsigned int port) {
+void server(int argc, char *argv[]) {
   int server_discr, client_discr; // File descriptors for sockets
   unsigned int clientSockSize;
   unsigned short int threadsState[MAX_CONN] = {0}; // Number of threads slots
@@ -72,7 +71,7 @@ void server(unsigned int port) {
   pthread_attr_init(&attr); // Initialize attributes
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 
-  server_discr = serverInitialize(port);
+  server_discr = serverInitialize(atoi(argv[2]));
 
   // Server runs infintly
   while (1) {
@@ -89,7 +88,8 @@ void server(unsigned int port) {
       if ((freeResource = checkAvailability(threadsState, MAX_CONN)) != -1) {
         passingArgs[freeResource].client_discr = client_discr; // Client's socket. It's just a number so there's no need to pass it b reference
         passingArgs[freeResource].finished = &threadsState[freeResource]; // Pointer to the address of thread's slot that will server this client
-        pthread_create(&(thread[freeResource]), &attr, clientThread, (void *) &passingArgs[freeResource]);
+				passingArgs[freeResource].RPC_Host = argv[1];
+				pthread_create(&(thread[freeResource]), &attr, clientThread, (void *) &passingArgs[freeResource]);
         fprintf(stdout, COLOR_CYAN "Resources found. Thread for client [%d] created successfully!\n" COLOR_RESET, client_discr);
       }
       else { // No free slots available
@@ -126,11 +126,29 @@ void *clientThread(void *arg) {
   unsigned int length;
   float average;
   int *minMaxNet;
-  int *minMax;
   char *muledMatrixStr;
   float *muledMatrix;
   unsigned int i;
+	unsigned short RPCflag;
 
+	//RPC variables
+	CLIENT *clnt;
+	float  *result_1;
+	intMatrix  findaverage_1_arg;
+	intMatrix  *result_2;
+	intMatrix  findminmax_1_arg;
+	floatMatrix  *result_3;
+	mulFloat  mulmatrixwithfloat_1_arg;
+
+	fprintf(stdout, COLOR_YELLOW "Establishing connection with RPC server for client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
+	#ifndef	DEBUG
+		clnt = clnt_create (passedArgs->RPC_Host, RPC_PROG, RPC_VERS, "udp");
+		if (clnt == NULL) {
+			clnt_pcreateerror (passedArgs->RPC_Host);
+			exit (1);
+		}
+	#endif	/* DEBUG */
+	fprintf(stdout, COLOR_BLUE "Connection established for client [%d] recieved\n" COLOR_RESET, passedArgs->client_discr);
 
   // Receive messages from a socket.
   // If no messages are available at the socket, it blocks the execution
@@ -172,44 +190,108 @@ void *clientThread(void *arg) {
   		}
   	}
 
+		RPCflag = 0;
+		switch(choice) {
+			case 1: {
+				fprintf(stdout, COLOR_YELLOW "Talking to RPC server from client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
+
+				findaverage_1_arg.numArray.numArray_len = numOfElements;
+				findaverage_1_arg.numArray.numArray_val = (int *)malloc(findaverage_1_arg.numArray.numArray_len * sizeof(int));
+				for (i = 0; i < findaverage_1_arg.numArray.numArray_len; i++) {
+					findaverage_1_arg.numArray.numArray_val[i] = numArray[i];
+				}
+
+				result_1 = findaverage_1(&findaverage_1_arg, clnt);
+				if (result_1 == (float *) NULL) {
+					clnt_perror (clnt, "call failed");
+				}
+				else {
+					RPCflag = 1;
+				}
+
+				free(findaverage_1_arg.numArray.numArray_val);
+				break;
+			}
+			case 2: {
+				fprintf(stdout, COLOR_YELLOW "Talking to RPC server from client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
+
+				findminmax_1_arg.numArray.numArray_len = numOfElements;
+				findminmax_1_arg.numArray.numArray_val = (int *)malloc(findminmax_1_arg.numArray.numArray_len * sizeof(int));
+				for (i = 0; i < findminmax_1_arg.numArray.numArray_len; i++) {
+					findminmax_1_arg.numArray.numArray_val[i] = numArray[i];
+				}
+
+				result_2 = findminmax_1(&findminmax_1_arg, clnt);
+				if (result_2 == (intMatrix *) NULL) {
+					clnt_perror (clnt, "call failed");
+				}
+				else {
+					RPCflag = 1;
+				}
+
+				free(findminmax_1_arg.numArray.numArray_val);
+				break;
+			}
+			case 3: {
+				fprintf(stdout, COLOR_YELLOW "Talking to RPC server from client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
+
+				mulmatrixwithfloat_1_arg.numArray.numArray_len = numOfElements;
+				mulmatrixwithfloat_1_arg.numArray.numArray_val = (int *)malloc(mulmatrixwithfloat_1_arg.numArray.numArray_len * sizeof(int));
+				for (i = 0; i < mulmatrixwithfloat_1_arg.numArray.numArray_len; i++) {
+					mulmatrixwithfloat_1_arg.numArray.numArray_val[i] = numArray[i];
+				}
+				mulmatrixwithfloat_1_arg.multiplier = multiplier;
+
+				result_3 = mulmatrixwithfloat_1(&mulmatrixwithfloat_1_arg, clnt);
+				if (result_3 == (floatMatrix *) NULL) {
+					clnt_perror (clnt, "call failed");
+				}
+				else {
+					RPCflag = 1;
+				}
+			}
+		}
+
   	switch(choice) {
   		case 1: {
-  			average = findAverage(numArray, numOfElements);
-        averageStr = (char *)malloc(100 * sizeof(char*));
-        sprintf(averageStr, "%f", average);
-        length = strlen(averageStr) + 1;
+				if (RPCflag) {
+					averageStr = (char *)malloc(100 * sizeof(char*));
+	        sprintf(averageStr, "%f", *result_1);
+	        length = strlen(averageStr) + 1;
 
-  			fprintf(stdout, COLOR_YELLOW "Sending message to client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
-        send(passedArgs->client_discr, &length, sizeof(unsigned int), 0);
-        send(passedArgs->client_discr, averageStr, length, 0);
+	  			fprintf(stdout, COLOR_YELLOW "Sending message to client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
+	        send(passedArgs->client_discr, &length, sizeof(unsigned int), 0);
+	        send(passedArgs->client_discr, averageStr, length, 0);
 
-        free(averageStr);
+	        free(averageStr);
+				}
+
         break;
   		}
   		case 2: {
-  			minMax = findMinMax(numArray, numOfElements);
-        minMaxNet = (int *)malloc(2 * sizeof(int));
-        minMaxNet[0] = htonl(minMax[0]);
-        minMaxNet[1] = htonl(minMax[1]);
+				if (RPCflag) {
+					minMaxNet = (int *)malloc(result_2->numArray.numArray_len * sizeof(int));
+	        minMaxNet[0] = htonl(result_2->numArray.numArray_val[0]);
+	        minMaxNet[1] = htonl(result_2->numArray.numArray_val[1]);
 
-  			fprintf(stdout, COLOR_YELLOW "Sending message to client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
-        send(passedArgs->client_discr, minMaxNet, 2 * sizeof(int), 0);
+	  			fprintf(stdout, COLOR_YELLOW "Sending message to client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
+	        send(passedArgs->client_discr, minMaxNet, 2 * sizeof(int), 0);
 
-  			free(minMax);
-  			free(minMaxNet);
+	  			free(minMaxNet);
+					free(result_2); // BUG may cause bug
+				}
 
         break;
   		}
   		case 3: {
-  			muledMatrix = mulMatrixWithFloat(numArray, numOfElements, multiplier);
-        muledMatrixStr = numArrayToCharArray(muledMatrix, numOfElements, ' ', &length, 0);
+        muledMatrixStr = numArrayToCharArray(result_3->muledArray.muledArray_val, result_3->muledArray.muledArray_len, ' ', &length, 0);
 
   			fprintf(stdout, COLOR_YELLOW "Sending message to client [%d]...\n" COLOR_RESET, passedArgs->client_discr);
         send(passedArgs->client_discr, &length, sizeof(unsigned int), 0);
         send(passedArgs->client_discr, muledMatrixStr, length, 0);
 
-  			free(muledMatrix);
   			free(muledMatrixStr);
+				free(result_3->muledArray.muledArray_val);
   		}
   	}
 
@@ -221,6 +303,10 @@ void *clientThread(void *arg) {
     choice = ntohl(choiceNet);
   }
 
+	#ifndef	DEBUG
+		fprintf(stdout, COLOR_GREEN "RPC connection for client [%d] closed successfully!\n" COLOR_RESET, passedArgs->client_discr);
+		clnt_destroy (clnt);
+	#endif	 /* DEBUG */
   *passedArgs->finished = 0; // Set this thread's slot as free
   fprintf(stdout, COLOR_GREEN "Connection with client [%d] closed successfully!\n" COLOR_RESET, passedArgs->client_discr);
   close(passedArgs->client_discr); // Closes the connection with the client's socket
@@ -283,55 +369,6 @@ int *strToIntArray(char *floatBuffer, unsigned int numOfElements) {
 	free(tmpStr);
 
 	return intArray;
-}
-
-/**
- * Find the average value of the numbers that client has sent
-*/
-float findAverage(int *numArray, int numOfElements) {
-	unsigned int sum = 0;
-	unsigned int i;
-
-	for (i = 0; i < numOfElements; sum += numArray[i++])
-  ;
-
-  // Considering the chance of dividing by 0
-  // Typecast to float to get float number
-  return (numOfElements > 0) ? sum / (float) numOfElements : 0;
-}
-
-int *findMinMax(int *numArray, unsigned int numOfElements) {
-	int *minMax = (int *)malloc(2 * sizeof(int));
-	unsigned int i;
-	int min;
-	int max;
-
-	min = numArray[0];
-	max = numArray[0];
-	for (i = 1; i < numOfElements; i++) {
-		if (min > numArray[i]) {
-			min = numArray[i];
-		}
-		if (max < numArray[i]) {
-			max = numArray[i];
-		}
-	}
-
-	minMax[0] = min;
-	minMax[1] = max;
-
-	return minMax;
-}
-
-float *mulMatrixWithFloat(int *numArray, unsigned int numOfElements, float multiplier) {
-	float *muledMatrix = (float *)malloc(numOfElements * sizeof(float));
-	unsigned int i;
-
-	for (i = 0; i < numOfElements; i++) {
-		muledMatrix[i] = numArray[i] * multiplier;
-	}
-
-	return muledMatrix;
 }
 
 char *numArrayToCharArray(void *voidArray, unsigned int numOfElements, char delimeter, unsigned int *length, unsigned short int mode) {
